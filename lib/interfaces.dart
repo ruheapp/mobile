24b60d439a327c7b1042f506e46f2840c77dc553
Anaïs Betts <anais@anaisbetts.org>
2019-11-08 20:58:26 -0800

    Tabula Rasa

--- lib/interfaces.dart
+++
@@ -1,9 +0,0 @@
-import 'package:firebase_auth/firebase_auth.dart';
-
-abstract class LoginManager {
-  UserInfo currentUser;
-
-  Future<UserInfo> login();
-  Future<void> logout();
-  Future<UserInfo> ensureNamedUser();
-}
